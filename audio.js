3ce84f7faafd95fd4db074800013f1b18345bbc4
Vyron Galapin <59787986+vdgalapin@users.noreply.github.com>
2020-03-09 11:25:15 -0500

    fix 1

---
+++ audio.js
@@ -0,0 +1,64 @@
+			function Qline1() {
+	                var audio = document.getElementById("s1");
+                    audio.play();
+	        }
+	            function Wline1() {
+	                var audio = document.getElementById("s2");
+                    audio.play();
+	        }
+	            function Eline1() {
+	                var audio = document.getElementById("s3");
+                    audio.play();
+	        }
+	            function Rline1() {
+	                var audio = document.getElementById("s4");
+                    audio.play();
+	        }
+	            function Aline2() {
+	                var audio = document.getElementById("s5");
+                    audio.play();
+	        }
+	            function Sline2() {
+	                var audio = document.getElementById("s6");
+                    audio.play();
+	        }
+	            function Dline2() {
+	                var audio = document.getElementById("s7");
+                    audio.play();
+	        }
+	            function Fline2() {
+	                var audio = document.getElementById("s8");
+                    audio.play();
+	        }
+	            function Zline3() {
+	                var audio = document.getElementById("s9");
+                    audio.play();
+	        }
+	            function Xline3() {
+	                var audio = document.getElementById("s10");
+                    audio.play();
+	        }
+	            function Cline3() {
+	                var audio = document.getElementById("s11");
+                    audio.play();
+	        }
+	            function Vline3() {
+	                var audio = document.getElementById("s12");
+                    audio.play();
+	        }
+	            function Tline4() {
+	                var audio = document.getElementById("s13");
+                    audio.play();
+	        }
+	            function Yline4() {
+	                var audio = document.getElementById("s14");
+                    audio.play();
+	        }
+	            function Uline4() {
+	                var audio = document.getElementById("s15");
+                    audio.play();
+	        }
+	            function Iline4() {
+	                var audio = document.getElementById("s16");
+                    audio.play();
+	        }
