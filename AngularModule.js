0f3c93514175bc1052c483a17728118f68bb5c8e
Vyron Galapin <59787986+vdgalapin@users.noreply.github.com>
2020-07-15 14:00:36 -0500

    Make it Cleaner
    Cleaning

---
+++ AngularModule.js
@@ -0,0 +1,19 @@
+angular.module('myApp', []).controller('myCtrl', function($scope) {
+    $scope.qline1 = " ";
+    $scope.wline1 = " ";
+    $scope.eline1 = " ";
+    $scope.rline1 = " ";
+    $scope.aline2 = " ";
+    $scope.sline2 = " ";
+    $scope.dline2 = " ";
+    $scope.fline2 = " ";
+    $scope.zline3 = " ";
+    $scope.xline3 = " ";
+    $scope.cline3 = " ";
+    $scope.vline3 = " ";
+    $scope.tline4 = " ";
+    $scope.yline4 = " ";
+    $scope.uline4 = " ";
+    $scope.iline4 = " ";
+})
+	  
